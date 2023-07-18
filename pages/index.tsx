b6ae5dcdfbf15e808a8a354dfcf31a359c25686a
sora <k.sora151201@gmail.com>
2023-07-18 18:55:09 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,51 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
-
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
-
-export default IndexPage
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+	initialImageUrl: string;
+};
+// ページコンポーネント関数にpropsを受け取る引数を追加する
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+	const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+	const [loading, setLoading] = useState(false); // 初期状態はfalseにしておく
+	// useEffect(() => {
+	//   fetchImage().then((newImage) => {
+	//     setImageUrl(newImage.url);
+	//     setLoading(false);
+	//   });
+	// }, []);
+	const handleClick = async () => {
+		setLoading(true);
+		const newImage = await fetchImage();
+		setImageUrl(newImage.url);
+		setLoading(false);
+	};
+	return (
+		<div className={styles.page}>
+			<button onClick={handleClick} className={styles.button}>
+				他のにゃんこも見る
+			</button>
+			<div className={styles.frame}>{loading || <img src={imageUrl} className={styles.img} />}</div>
+		</div>
+	);
+};
+export default IndexPage;
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+	const image = await fetchImage();
+	return {
+		props: {
+			initialImageUrl: image.url,
+		},
+	};
+};
+type Image = {
+	url: string;
+};
+const fetchImage = async (): Promise<Image> => {
+	const res = await fetch("https://api.thecatapi.com/v1/images/search");
+	const images = await res.json();
+	console.log(images);
+	return images[0];
+};
